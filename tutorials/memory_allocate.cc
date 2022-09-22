eb239a83624a705b1226ab10544de889ca9b4d14
wangrl2016 <wangrl2016@gmail.com>
2022-09-22 10:47:11 +0800

    Add metronome

--- tutorials/memory_allocate.cc
+++ tutorials/memory_allocate.cc
@@ -3,6 +3,19 @@
 //
 
 int main([[maybe_unused]] int argc, [[maybe_unused]] char* argv[]) {
+    // 1. 简单介绍开发环境
+    // 电脑配置和开发工具链
+
+    // 2. man malloc查看内存管理函数族
+    // calloc, free, malloc, realloc, reallocf, valloc, aligned_alloc – memory allocation
+
+    // 3. malloc/free函数介绍
+
+    // 4. calloc函数介绍
+
+    // 5. realloc函数介绍
+
+    // 6. 内存操作函数memset/memcpy介绍
 
     return 0;
 }
