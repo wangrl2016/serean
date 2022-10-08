3b3067ddfd8fb430ea36475fbe5d02a94c951686
wangrl2016 <wangrl2016@gmail.com>
2022-10-08 08:48:50 +0800

    Smart pointer

--- tutorials/smart_pointer.cc
+++ tutorials/smart_pointer.cc
@@ -29,6 +29,60 @@ namespace internal {
         float* score_ = nullptr;
         int count_ = 0;
     };
+
+    // RefCnt is the base class for objects that may be shared by multiple
+    // objects. When an existing owner wants to share a reference, it calls ref().
+    // When an owner wants to release its reference, it calls unref(). When the
+    // shared object's reference count goes to zero as the result of an unref()
+    // call, its (virtual) destructor is called. It is an error for the
+    // destructor to be called explicitly (or via the object going out of scope on
+    // the stack or calling delete) if GetRefCnt() > 1.
+    class RefCnt {
+    public:
+        RefCnt(RefCnt&&) = delete;
+
+        RefCnt(const RefCnt&) = delete;
+
+        RefCnt& operator=(RefCnt&&) = delete;
+
+        RefCnt& operator=(const RefCnt&) = delete;
+
+        // Default construct, initializing the reference count to 1.
+        RefCnt() : ref_cnt_(1) {}
+
+        // destruct
+        virtual ~RefCnt() = default;
+
+        bool Unique() const {
+            if (1 == ref_cnt_.load(std::memory_order_acquire)) {
+                // Acquire barrier is only really needed if we return
+                // true. It prevents code conditioned on the result of
+                // Unique() from running until previous owners are all
+                // totally done calling unref().
+                return true;
+            }
+            return false;
+        }
+
+        void Ref() const {
+            // No barrier required.
+            ref_cnt_.fetch_add(+1, std::memory_order_relaxed);
+        }
+
+        // Decrement the reference count. If the reference count is 1
+        // before the decrement, then delete the object. Note that
+        // if this is the case, then the object needs to have been
+        // allocated via new, and not on the stack.
+        void Unref() const {
+
+        }
+
+    private:
+        mutable std::atomic<int32_t> ref_cnt_;
+
+
+
+    };
 }
 
 int main([[maybe_unused]] int argc, [[maybe_unused]] char* argv[]) {
@@ -50,7 +104,12 @@ int main([[maybe_unused]] int argc, [[maybe_unused]] char* argv[]) {
         if (student3 == nullptr) {
             std::cout << "After move() function, student3 is nullptr" << std::endl;
         }
+        // 此时如果替换成为student3->count()程序会发生崩溃
         std::cout << "student4 count " << student4->count() << std::endl;
+    }
+
+    {
+        std::cout << "## 自定义引用计数" << std::endl;
     }
     return 0;
 }
